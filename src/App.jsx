0ed465ba59d10e8ea11d83a439f56af59436e591
Rahul Choudhary <rc9782276591@gmail.com>
2023-04-21 07:40:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/about'  element={<About/>}/>
+          <Route path='/cocktail/:id'  element={<SingleCocktail/>}/>
+          <Route path='*'  element={<Error/>}/>
+
+
+          </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className="section-title">About us</h1>
+      <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quam tenetur, mollitia aliquam eum illo cumque maiores modi, aut nostrum repudiandae tempora necessitatibus quaerat porro incidunt deserunt, quidem culpa excepturi.
+      </p>
+    </section>
+  )
+}
+
+export default About
